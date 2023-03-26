cde02f58886002de271df07eb466310d0e24ebfd
Omochice <mochice.mls.ntl@gmail.com>
2023-03-26 09:52:09 +0900

    :+1: apply prismjs highlight

--- _includes/base.tsx
+++ _includes/base.tsx
@@ -1,19 +1,18 @@
 import { SharedHead } from "./components/sharedHead.tsx";
 import { ZennHeader } from "./components/zennHeader.tsx";
-
-const rootCss = {
-  // fontSize: "30px"
-}
+import { PrismHighlight } from "./components/prismHighlight.tsx"
 
 export default ({ title, children, type }) => (
   <>
-    <html lang="ja" style={ rootCss }>
+    <html lang="ja">
       <head>
         <SharedHead />
         <ZennHeader />
+        <PrismHighlight />
+        <link rel="stylesheet" href="/assets/style.css" />
         <title>{title}</title>
       </head>
-      <body className="p-10" style={ rootCss }>{children}</body>
+      <body className="p-10">{children}</body>
     </html>
   </>
 );
---
+++ _includes/components/prismHighlight.tsx
@@ -0,0 +1,10 @@
+export function PrismHighlight() {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/prismjs@1.29.0/themes/prism-tomorrow.min.css"
+      />
+    </>
+  );
+}
