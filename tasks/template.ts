ef7fa173064d1d097cd1c858499dbc7d19fc67fe
Omochice <mochice.mls.ntl@gmail.com>
2023-06-13 19:13:27 +0900

    :gear: Add task to generate template

---
+++ tasks/template.ts
@@ -0,0 +1,26 @@
+import { stringify } from "https://deno.land/std@0.191.0/yaml/mod.ts";
+import { format } from "npm:date-fns@2.3.0";
+import { join } from "https://deno.land/std@0.183.0/path/mod.ts";
+
+function generate() {
+  const frontMatter = {
+    layout: "zenn.tsx",
+    title: "",
+    topics: [],
+    excerpt: "",
+    type: "tech",
+  };
+  return ["---", stringify(frontMatter), "---"]
+    .map((e) => e.trim())
+    .join("\n");
+}
+
+if (import.meta.main) {
+  const filename =
+    [format(new Date(), "yyyy-MM-dd"), crypto.randomUUID()].join("-") +
+    ".md";
+  const outPath = join(Deno.cwd(), "posts", filename);
+
+  Deno.writeTextFileSync(outPath, generate());
+  console.log(`generate template to ${outPath}`)
+}
