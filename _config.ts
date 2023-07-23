71a1af7a74ffdbb7edb51c9742fe7ca9883cd39e
Omochice <mochice.mls.ntl@gmail.com>
2023-07-23 23:42:57 +0900

    :package: replace windi to tailwind

--- _config.ts
+++ _config.ts
@@ -1,22 +1,22 @@
 import lume from "lume/mod.ts";
-import windi from "lume/plugins/windi_css.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import postcss from "lume/plugins/postcss.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
 import zennRenderer from "./plugins/zenn-renderer/mod.ts";
 import zennKatex from "./plugins/zenn-katex/mod.ts";
-import pagefind from "lume/plugins/pagefind.ts";
 
 const site = lume({
   location: new URL("https://omochice.github.io/blog/"),
 });
 
-site.use(jsx())
-  .use(windi())
+site
+  .use(tailwindcss())
+  .use(postcss())
+  .use(jsx())
   .use(zennRenderer())
   .use(zennKatex())
   .use(resolveUrls())
-  .use(pagefind())
-  .copy("/assets");
 
 export default site;
 
