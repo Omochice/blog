f33c3dcb2f05492f278138c1bbe57677ffc718a5
Omochice <mochice.mls.ntl@gmail.com>
2023-08-01 08:58:45 +0900

    refactor: apply centering size

--- site/_includes/zenn.tsx
+++ site/_includes/zenn.tsx
@@ -12,18 +12,18 @@ export default ({ title, children, topics, emoji }: ZennPageData) => (
   <>
     <a href={joinUrl("/")}>&lt; Return to index</a>
     <div className="flex justify-center flex-col gap-y-4">
-      <h1 className="text-5xl">
-        {title} {emoji}
-      </h1>
-      <div>
-        {(topics ?? []).map((topic) => (
-          <TopicButton
-            topic={topic}
-            key={topic}
-          />
-        ))}
-      </div>
       <main className="overflow-ellipsis" style={contentWidth}>
+        <h1 className="text-5xl">
+          {title} {emoji}
+        </h1>
+        <div>
+          {(topics ?? []).map((topic) => (
+            <TopicButton
+              topic={topic}
+              key={topic}
+            />
+          ))}
+        </div>
         <article className="znc leading-loose">{children}</article>
       </main>
     </div>
