02e60c89dcdd210e627013a9e93006b216b96186
Omochice <mochice.mls.ntl@gmail.com>
2023-08-14 09:42:29 +0900

    fix: specify version

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,7 +1,7 @@
-import { rules as standard } from "npm:stylelint-config-standard";
-import { rules as tailwind } from "npm:stylelint-config-tailwindcss";
-import { rules as recessOrder } from "npm:stylelint-config-recess-order";
-import { default as order } from "npm:stylelint-order";
+import { rules as standard } from "npm:stylelint-config-standard@34.0.0";
+import { rules as tailwind } from "npm:stylelint-config-tailwindcss@0.0.7";
+import { rules as recessOrder } from "npm:stylelint-config-recess-order@4.3.0";
+import { default as order } from "npm:stylelint-order@6.0.3";
 
 export default {
   rules: {
