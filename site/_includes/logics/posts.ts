553f77c97fe493777f7b019a6166eebb4c969b18
denopendabot <denopendabot@github.com>
2023-08-25 01:06:45 +0000

    build(deps): bump std from 0.198.0 to 0.200.0

--- site/_includes/logics/posts.ts
+++ site/_includes/logics/posts.ts
@@ -1,7 +1,7 @@
 import type { Page } from "lume/core.ts";
 import site from "../../../_config.ts";
 import { compareDesc, fromUnixTime, parse } from "date-fns";
-import { basename } from "https://deno.land/std@0.198.0/path/posix.ts";
+import { basename } from "https://deno.land/std@0.200.0/path/posix.ts";
 import { err, ok, Result } from "npm:neverthrow@6.0.1-0";
 
 function comparePage(a: Page, b: Page): number {
--- tasks/template.ts
+++ tasks/template.ts
@@ -1,6 +1,6 @@
-import { stringify } from "https://deno.land/std@0.198.0/yaml/mod.ts";
+import { stringify } from "https://deno.land/std@0.200.0/yaml/mod.ts";
 import { format } from "npm:date-fns@2.30.0";
-import { join } from "https://deno.land/std@0.198.0/path/mod.ts";
+import { join } from "https://deno.land/std@0.200.0/path/mod.ts";
 
 function generate() {
   const frontMatter = {
