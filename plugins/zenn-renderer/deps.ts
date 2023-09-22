3b6e6c916f88af677f29ccdca5250cc2f16e68ab
denopendabot <denopendabot@github.com>
2023-09-22 01:07:56 +0000

    build(deps): bump zenn-markdown-html from 0.1.145-alpha.0 to 0.1.146

--- plugins/zenn-renderer/deps.ts
+++ plugins/zenn-renderer/deps.ts
@@ -14,5 +14,5 @@ export {
 export type { Parser } from "https://deno.land/std@0.202.0/front_matter/mod.ts";
 export { parse as parseYAML } from "https://deno.land/std@0.202.0/yaml/parse.ts";
 // export { default as markdownToHtml } from "https://esm.sh/zenn-markdown-html@0.1.134?bundle";
-import zenn from "npm:zenn-markdown-html@0.1.145-alpha.0";
+import zenn from "npm:zenn-markdown-html@0.1.146";
 export const markdownToHtml = zenn.default;
