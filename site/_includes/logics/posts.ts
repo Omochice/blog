c75f7b000271e5c651b201051579c19bfdf40e88
Omochice <mochice.mls.ntl@gmail.com>
2023-10-26 11:32:41 +0900

    Revert "chore(deps): update dependency neverthrow to v6.1.0 (#57)"
    This reverts commit a134ba5fd7a08a7d81be28e80b321008b57c556f.

--- site/_includes/logics/posts.ts
+++ site/_includes/logics/posts.ts
@@ -2,7 +2,7 @@ import type { Page } from "lume/core.ts";
 import site from "../../../_config.ts";
 import { compareDesc, fromUnixTime, parse } from "date-fns";
 import { basename } from "https://deno.land/std@0.204.0/path/posix.ts";
-import { err, ok, Result } from "npm:neverthrow@6.1.0-0";
+import { err, ok, Result } from "npm:neverthrow@6.0.1-0";
 
 function comparePage(a: Page, b: Page): number {
   const defaultCreated = fromUnixTime(0);
