91ea732539d95fdb2b967f331e55b90f748157ad
Omochice <mochice.mls.ntl@gmail.com>
2024-01-17 01:14:09 +0900

    chore: rename template

--- site/_includes/article.tsx
+++ site/_includes/article.tsx
@@ -1,4 +1,4 @@
-import type { ZennPageData } from "../plugins/zenn-renderer/mod.ts";
+import type { Data } from "lume/core.ts";
 import { TopicButton } from "./topicButton.tsx";
 import { joinUrl } from "./logics/joinUrl.ts";
 import SuggestButton from "./components/suggestButton.tsx";
@@ -9,12 +9,12 @@ const contentWidth = {
   width: "max(80%, min(100%, 1024px))",
 };
 
-export default ({ title, children, topics, emoji, page }: ZennPageData) => (
+export default ({ title, children, topics, page }: Data) => (
   <>
     <a href={joinUrl("/")}>&lt; Return to index</a>
     <div className="flex justify-center flex-col gap-y-4">
       <h1 className="text-5xl">
-        {title} {emoji}
+        {title}
       </h1>
       <div className="flex gap-2">
         {(topics ?? []).map((topic) => (
@@ -25,9 +25,9 @@ export default ({ title, children, topics, emoji, page }: ZennPageData) => (
         ))}
       </div>
       <main className="overflow-ellipsis" style={contentWidth}>
-        <article className="znc leading-loose">{children}</article>
+        <article className="leading-loose article">{children}</article>
       </main>
-      <SuggestButton path={page.src.entry.path} />
+      <SuggestButton path={page?.src.entry?.path ?? ""} />
     </div>
   </>
 );
