09a574182f2e599a65f995a7f8ce863bf1d584fb
Omochice <mochice.mls.ntl@gmail.com>
2024-11-30 18:09:05 +0900

    chore: format command

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
+import mdx from "@astrojs/mdx";
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,6 @@
+import { rules as recessOrder } from "npm:stylelint-config-recess-order@4.3.0";
 import { rules as standard } from "npm:stylelint-config-standard@34.0.0";
 import { rules as tailwind } from "npm:stylelint-config-tailwindcss@0.0.7";
-import { rules as recessOrder } from "npm:stylelint-config-recess-order@4.3.0";
 import { default as order } from "npm:stylelint-order@6.0.3";
 
 export default {
