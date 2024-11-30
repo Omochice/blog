8def5f8189bc2209bb6cb8ad127d7e19854e6bb0
Omochice <mochice.mls.ntl@gmail.com>
2024-11-30 18:32:34 +0900

    test: fix desc

--- src/lib/is-test-year.test.ts
+++ src/lib/is-test-year.test.ts
@@ -7,7 +7,7 @@ describe("isTestYear", () => {
       true,
     );
   });
-  it("should return true. If the dates have less than 1 year", () => {
+  it("should return false. If the dates have less than 1 year", () => {
     expect(isPastYear(new Date("2022-01-01"), new Date("2022-12-31"))).toBe(
       false,
     );
