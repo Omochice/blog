b87089d1ceb25847bcc620ac8942037cbc12d6e8
Omochice <44566328+Omochice@users.noreply.github.com>
2024-12-01 02:25:06 +0900

    fix: fix post head (#378)
    * chore: add ufo
    
    * fix: fix links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Omochice.github.io",
-  base: process.env.CI ? "/blog" : "/",
+  site: "https://Omochice.github.io/blog",
+  base: "/blog",
   integrations: [mdx(), sitemap()],
 });
