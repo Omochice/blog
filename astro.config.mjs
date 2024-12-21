68dd08b1c57df0fcec53519398832f6ab173efc5
Omochice <mochice.mls.ntl@gmail.com>
2024-12-22 01:30:34 +0900

    revert: "feat: add budoux markdown plugin"
    This reverts commit b0c54d212049bb8160ca947603beec072960695c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,12 @@
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 // @ts-check
 import { defineConfig } from "astro/config";
-import { loadDefaultJapaneseParser } from "budoux";
-import { visit } from "unist-util-visit";
 
-function remarkBudoux() {
-  const parser = loadDefaultJapaneseParser();
-  return (tree) => {
-    visit(tree, "text", (node) => {
-      if (node.value.trim() === "") {
-        return;
-      }
-      node.value = parser.parse(node.value).join("<wbr />");
-      node.type = "html";
-    });
-  };
-}
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Omochice.github.io/blog",
   base: "/blog",
   integrations: [mdx(), sitemap()],
-  markdown: {
-    remarkPlugins: [remarkBudoux],
-  },
 });
