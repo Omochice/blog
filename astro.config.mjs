b0c54d212049bb8160ca947603beec072960695c
Omochice <mochice.mls.ntl@gmail.com>
2024-12-22 01:15:13 +0900

    feat: add budoux markdown plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,29 @@
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 // @ts-check
 import { defineConfig } from "astro/config";
+import { loadDefaultJapaneseParser } from "budoux";
+import { visit } from "unist-util-visit";
 
-import sitemap from "@astrojs/sitemap";
+function remarkBudoux() {
+  const parser = loadDefaultJapaneseParser();
+  return (tree) => {
+    visit(tree, "text", (node) => {
+      if (node.value.trim() === "") {
+        return;
+      }
+      node.value = parser.parse(node.value).join("<wbr />");
+      node.type = "html";
+    });
+  };
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Omochice.github.io/blog",
   base: "/blog",
   integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkBudoux],
+  },
 });
