addfeb6623f7a6c51cc410aaf7a246391a7d0265
Omochice <44566328+Omochice@users.noreply.github.com>
2025-03-09 18:04:14 +0900

    feat: show `#` on article as link (#564)
    * chore: add rehype-autolink-headings
    
    * feat: show article heading as link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,34 @@
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import sitemap from "@astrojs/sitemap";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Omochice.github.io/blog",
   base: "/blog",
   integrations: [mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          content: {
+            type: "text",
+            value: "#",
+          },
+          headingProperties: {
+            className: ["anchor"],
+          },
+          properties: {
+            className: ["anchor-link"],
+          },
+        },
+      ],
+    ],
+  },
 });
