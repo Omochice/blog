c1f1f01b25a6fe4986a68be9d2439daf3893c873
Omochice <44566328+Omochice@users.noreply.github.com>
2025-03-09 17:20:24 +0900

    chore: add stylelint (#563)
    * chore: add stylelint
    
    * style: fmt css
    
    * chore: add an adaptor for astro
    
    * style: fmt astro style tag
    
    * chore: add check task

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,14 +1,12 @@
-import { rules as recessOrder } from "npm:stylelint-config-recess-order@4.3.0";
-import { rules as standard } from "npm:stylelint-config-standard@34.0.0";
-import { rules as tailwind } from "npm:stylelint-config-tailwindcss@0.0.7";
-import { default as order } from "npm:stylelint-order@6.0.3";
-
 export default {
-  rules: {
-    ...standard,
-    ...recessOrder,
-    ...tailwind,
-  },
-  plugins: [order],
-  ignoreFiles: ["_site/**/*"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "stylelint-config-astro",
+  ],
+  overrides: [
+    {
+      files: ["*.astro", "**/*.astro"],
+    },
+  ],
 };
