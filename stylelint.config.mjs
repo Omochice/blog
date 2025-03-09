98fabc9bd60496f626e2eed21aa9049c995d92cb
Omochice <mochice.mls.ntl@gmail.com>
2025-03-09 17:07:34 +0900

    chore: add an adaptor for astro

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,3 +1,12 @@
 export default {
-  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "stylelint-config-astro",
+  ],
+  overrides: [
+    {
+      files: ["*.astro", "**/*.astro"],
+    },
+  ],
 };
