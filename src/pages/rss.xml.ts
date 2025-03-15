4733cdab20b3f8ed70b4927c93505df9ceb847ba
Omochice <mochice.mls.ntl@gmail.com>
2025-03-15 19:45:46 +0900

    feat: add content to rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,7 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
+import { experimental_AstroContainer } from "astro/container";
+import sanitizeHtml from "sanitize-html";
 import { joinURL } from "ufo";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
@@ -9,15 +11,25 @@ type Context = {
 
 export async function GET(context: Context) {
   const posts = await getCollection("post");
+  const container = await experimental_AstroContainer.create();
+  const items = await Promise.all(
+    posts.map(async (post) => ({
+      ...post.data,
+      link: joinURL(import.meta.env.BASE_URL, "posts", post.slug),
+      pubDate: post.data.date,
+      content: sanitizeHtml(
+        await container.renderToString((await post.render()).Content),
+        {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+        },
+      ),
+    })),
+  );
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: joinURL(import.meta.env.BASE_URL, "posts", post.slug),
-      pubDate: post.data.date,
-      description: post.data.description || post.body,
-    })),
+    items: items.sort((a, b) => b.date.valueOf() - a.date.valueOf()),
   });
 }
