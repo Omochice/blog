2e02c816d1cc2f0f66142ebd93b4b76fef0ab24f
Omochice <mochice.mls.ntl@gmail.com>
2025-09-27 22:44:48 +0900

    feat: add component to link original source

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
 export const SITE_TITLE = "$ Omochice 2>&1";
 export const SITE_DESCRIPTION = "Omochice's stdout and stderr";
 export const NAME = "Omochice";
+export const REPOSITORY = new URL("https://github.com/Omochice/blog");
+export const DEFAULT_BRANCH = "main";
