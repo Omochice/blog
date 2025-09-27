cd07369df6c7195399609e63e3d89fd36776772b
Omochice <44566328+Omochice@users.noreply.github.com>
2025-09-27 21:11:52 +0900

    build: use lightning css (#1006)
    * chore: add lightningcss
    
    * build: enable lightningcss
    
    * refactor: move into browserslistrc

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 
@@ -9,6 +11,17 @@ export default defineConfig({
   site: "https://Omochice.github.io/blog",
   base: "/blog",
   integrations: [mdx(), sitemap()],
+  vite: {
+    css: {
+      transformer: "lightningcss",
+      lightningcss: {
+        targets: browserslistToTargets(browserslist()),
+      },
+    },
+    build: {
+      cssMinify: "lightningcss",
+    },
+  },
   markdown: {
     rehypePlugins: [
       rehypeSlug,
